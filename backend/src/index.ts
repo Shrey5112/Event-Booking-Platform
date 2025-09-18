9343dac1c96274405e8fbacc69b969aa28d28654
Shrey <shahshrey5112@gmail.com>
2025-09-18 14:30:53 +0530

    modify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(cors({ origin: "*", credentials: true }));
-const allowedOrigins = ["http://localhost:3000", "https://event-booking-platform-mocha.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "https://event-booking-platform-mocha.vercel.app"];
 
 app.use(cors({
   origin: (origin, callback) => {
