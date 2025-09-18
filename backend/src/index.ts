5d7e8c67821a353483f5efa38d1f609f9f0aa561
Shrey <shahshrey5112@gmail.com>
2025-09-18 14:17:33 +0530

    modify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,19 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "*", credentials: true }));
+const allowedOrigins = ["http://localhost:3000", "https://event-booking-platform-mocha.vercel.app/"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 // Routes
 app.use("/api/auth", authRoutes);
