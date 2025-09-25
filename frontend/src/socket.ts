f2bb6dcf82958862769aa4ec91327a82e4d43c76
Shrey <shahshrey5112@gmail.com>
2025-09-25 18:02:25 +0530

    modify

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,12 +1,9 @@
 // src/socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000/bookings", {
+const socket = io(`${import.meta.env.VITE_SOCKET_URL}/bookings`, {
   withCredentials: true,
-  // auth: (cb) => {
-  //   // token is already in cookie (httpOnly), so just pass
-  //   cb({}); 
-  // },
+  transports: ["websocket"],
 });
 
 export default socket;
